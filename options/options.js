c05bac8e9e83916478d39c9046cd4123a8d6b05f
Crowdbotics <team@crowdbotics.com>
2021-10-04 21:54:54 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "resurgence_staythec_31100",
+  url: "https://resurgence_staythec_31100.botics.co",
+  api: "https://resurgence_staythec_31100.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
